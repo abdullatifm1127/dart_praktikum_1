1630fdb94375af86338c254d918e7a9fd000224a
latif <mabdullatifm1127@gmail.com>
2025-10-01 13:14:36 +0700

    Praktikum mobile

---
+++ bin/Praktikum 4/P2.dart
@@ -0,0 +1,4 @@
+void main(){
+  var halogens = {'fluorine', 'cholrine', 'bromine'};
+  print(halogens);
+}
---
+++ bin/Praktikum 4/P3.dart
@@ -0,0 +1,14 @@
+void main(){
+  var gifts ={
+    'first' : 'partridge',
+    'second': 'turtledoves',
+    'fifth' : 1
+  };
+  var nobleGases ={
+    1: 'helium',
+    2: 'neon',
+    3: 2
+  };
+  print(gifts);
+  print(nobleGases);
+}
