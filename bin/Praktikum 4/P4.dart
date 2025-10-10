54b596f4988c4def9274dda3dda83a844cf8d3d7
latif <mabdullatifm1127@gmail.com>
2025-10-10 21:56:55 +0700

    Tugas praktikum

--- bin/Praktikum 4/P4.dart
+++ bin/Praktikum 4/P4.dart
@@ -26,4 +26,9 @@ var promoActive1 = false;
 var login = 'Manager';
 var nav2 = ['Home', 'Furniture', 'Plants', if (login == 'Manager') 'Iventory'];
   print(nav2);
+
+  var listOfInts = [1, 2, 3];
+  var listOfStrings = ['#0', for (var i in listOfInts) '#$i'];
+  assert(listOfStrings[1] == '#1');
+  print(listOfStrings);
 }
---
+++ bin/Praktikum 4/Tugas.dart
@@ -0,0 +1,10 @@
+void tampilData(String nama, int umur, String alamat) {
+  print('Nama   : $nama');
+  print('Umur   : $umur tahun');
+  print('Alamat : $alamat');
+}
+
+void main() {
+
+  tampilData('Abdul', 20, 'Malang');
+}
