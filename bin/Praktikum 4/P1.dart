7d01c000e6758982740039be7d0e53551aa3b54d
latif <mabdullatifm1127@gmail.com>
2025-10-10 20:48:47 +0700

    Praktikum 4 langkah 3

--- bin/Praktikum 4/P1.dart
+++ bin/Praktikum 4/P1.dart
@@ -1,12 +1,7 @@
 void main (){
-  var list = [1,2,3];
-  assert(list.length == 3);
-  assert(list[1] == 2);
-  print(list.length);
-  print(list[1]);
+  final List<String?> list = List.filled(5, null);
 
-  list[1] = 1;
-  assert(list[1] == 1);
-  print(list[1]);
-
+    list[1] = "Abdul Latif Mukhlisin";
+    list[2] = "2341720206";
+    print(list);
 }
