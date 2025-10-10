85cee51fefd183e759de68609683c3a5573ea608
latif <mabdullatifm1127@gmail.com>
2025-10-10 21:10:48 +0700

    Praktikum 4 Langkah 3

--- bin/Praktikum 4/P2.dart
+++ bin/Praktikum 4/P2.dart
@@ -1,4 +1,16 @@
 void main(){
   var halogens = {'fluorine', 'cholrine', 'bromine'};
   print(halogens);
+
+  var names1 = <String>{};
+Set<String> names2 = {}; 
+
+var names3 = {}; 
+
+names1.add("Abdul Latif Mukhlisin");
+names2.add("2341720206");
+
+print(names1);
+print(names2);
+print(names3);
 }
