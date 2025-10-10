6d4ea970c7b5b1b9d10a21aa84556f5680466ada
latif <mabdullatifm1127@gmail.com>
2025-10-10 21:32:42 +0700

    Praktikum 4 Langkah 4

--- bin/Praktikum 4/P4.dart
+++ bin/Praktikum 4/P4.dart
@@ -4,4 +4,22 @@ void main(){
   print(list1);
   print(list2);
   print(list2.length);
+
+  var list = [1,2, null];
+  print(list1);
+  var list3 = [0, ...?list1];
+  print(list3.length);
+
+  var nim = ['2', '3', '4', '1', '7', '2','0', '2', '0', '6'];
+  var listnim = [...nim];
+  print(listnim);
+  print(listnim.length);
+
+var promoActive = true;
+  var nav = ['Home', 'Furniture', 'Plants', if (promoActive)'Outlet'];
+print(nav);
+
+var promoActive1 = false;
+  var nav1 = ['Home', 'Furniture', 'Plants', if (promoActive1) 'Outlet'];
+  print(nav1);
 }
